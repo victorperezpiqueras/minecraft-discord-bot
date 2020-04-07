b0ec126e00b995f644e0f946b59e538c6ce6aef8
victorperezpiqueras <victorperezpiqueras@gmail.com>
2020-04-07 19:07:20 +0200

    agregados juegos del tonto aleatorios y por voz

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ function playAudio(msg, audioFile) {
 
 bot.on('ready', () => {
   console.log('Bot online');
+  bot.user.setActivity("de chavaleo");
 });
 
 bot.on('message', msg => {
@@ -58,6 +59,10 @@ bot.on('message', msg => {
         .addField('Server info: ', '!server')
         .addField('Chavalada online: ', '!online')
         .addField('Lista de audios: ', '!audiolist')
+        .addField('El juego del tonto: ', '!tonto')
+        .addField('El juego del tonto escrito a mano con espacios: ', '!tontoamano')
+        .addField('El juego del tonto por voz: ', '!tontovoz')
+        .addField('Mayor o menor: ', '!mayormenor')
         .setColor(0x00AE86)
       msg.channel.send(embed);
       break;
@@ -159,24 +164,138 @@ bot.on('message', msg => {
     case 'valkirias':
       playAudio(msg, 'valkirias');
       break;
+    /* case 'probando':
+      console.log("hola")
+      break; */
+
+    case 'tonto':
+      var members = msg.member.voice.channel.members;
+      var random = Math.floor(Math.random() * members.size);
+      var c = 0;
+      members.forEach(member => {
+        if (random == c) {
+          msg.channel.send("El tonto es: <@" + member.user.id + ">");
+        }
+        c++;
+      })
+      break;
+
+    case 'tontoamano':
+      var members = msg.member.voice.channel.members;
+      var random = Math.floor(Math.random() * (args.length - 1)) + 1;
+      msg.channel.send("El tonto es: " + args[random] + "");
+      break;
+
+    case 'tontovoz':
+      var members = msg.member.voice.channel.members;
+      var random = Math.floor(Math.random() * (args.length - 1)) + 1;
+      if (args[random]) {
+        const gtts = require('gtts.js').gTTS;
+        const speech = new gtts(args[random], 'it')
+        speech.save("./audios/output.mp3")
+          .then(function () {
+            msg.member.voice.channel.join()
+              .then(connection => {
+                connection.play('./audios/output.mp3');
+              })
+          }).catch(function (err) {
+
+          })
+      }
+      break;
+
+    case 'mayormenor':
+      // inicializar 
+      var cartas = "1 1 1 1 2 2 2 2 3 3 3 3 4 4 4 4 5 5 5 5 6 6 6 6 7 7 7 7 10 10 10 10 11 11 11 11 12 12 12 12";
+      cartas = cartas.split(' ');
+      console.log(cartas)
+      var random = Math.floor(Math.random() * (cartas.length - 1)) + 1;
+      var cartaRandom = Number(cartas[random]);
+      var cartaVieja = Number(cartas[random]);
+      var tragos = 0;
+      // mostrar carta 
+      msg.channel.send("Ha salido: " + cartaRandom);
+      //mostar cartas totales 
+      cartas.splice(random, 1);
+      msg.channel.send("Cartas totales: " + cartas);
+
+
+      msg.channel.send("¿Mayor o menor?");
+      var respuesta = "";
+      // bot.on('message', msg => { 
+      bot.on('message', msg => {
+        /* while (respuesta !== "mayor" || respuesta !== "menor") { */
+        if (msg.author.equals(bot.user)) return;
+
+        if (cartas.length == 0) return;
+
+        respuesta = msg.content.toLowerCase();
+        /*  } */
+
+
+
+        // carta aleatoria 
+        var random = Math.floor(Math.random() * (cartas.length - 1)) + 1;
+        cartaVieja = Number(cartaRandom);
+        cartaRandom = Number(cartas[random]);
+        console.log("random=", cartaRandom)
+        console.log("vieja=", cartaVieja)
+        // mostrar carta
+        msg.channel.send("Ha salido: " + cartaRandom);
+
+        console.log(Number(cartaRandom))
+        console.log(Number(cartaVieja))
+        cartas.splice(random, 1);
+        if (respuesta === "mayor") {
+          if (Number(cartaRandom) > Number(cartaVieja)) {
+            msg.channel.send("Te salvaste pinche");
+            tragos++;
+          } else {
+            msg.channel.send("A beber " + tragos + " tragos");
+            tragos = 0;
+          }
+        }
+        else if (respuesta === "menor") {
+          if (Number(cartaRandom) < Number(cartaVieja)) {
+            msg.channel.send("Te salvaste pinche");
+            tragos++;
+          } else {
+            msg.channel.send("A beber " + tragos + " tragos");
+            tragos = 0;
+          }
+        }
+        // mostar cartas totales 
+        msg.channel.send("Cartas totales: " + cartas);
+        msg.channel.send("¿Mayor o menor?");
+      });
+      break;
   }
 
 });
+/*
+newUserChannel.join().then(connection => {
+      connection.play('./audios/ts3entered.mp3');
+     }) 
+ */
 
-/* const mcBot = mc.createClient({
-  host: "",   // optional
-  port: 25616,         // optional
-  username: "MCBot",
-});
-mcBot.on('chat', function (packet) {
-  // Listen for chat messages and echo them back.
-  var jsonMsg = JSON.parse(packet.message);
-  if (jsonMsg.translate == 'chat.type.announcement' || jsonMsg.translate == 'chat.type.text') {
-    var username = jsonMsg.with[0].text;
-    var msg = jsonMsg.with[1];
-    if (username === client.username) return;
-    client.write('chat', { message: msg });
+/* bot.on('voiceStateUpdate', (oldMember, newMember) => {
+  let newUserChannel = newMember.voiceChannel
+  let oldUserChannel = oldMember.voiceChannel
+
+  if (newUserChannel !== undefined) {
+    console.log("play")
+    newMember.voice.channel.join().then(connection => {
+      console.log("play")
+      connection.play('./audios/ts3entered.mp3');
+    })
   }
-}); */
+
+  if (oldUserChannel === undefined && newUserChannel !== undefined) {
+
+    // User Joins a voice channel
+    console.log("joined")
+
+  } 
+}) */
 
 bot.login(process.env.DISCORD_TOKEN);
