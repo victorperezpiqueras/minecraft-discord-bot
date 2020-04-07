8dd746eb35b150cb269c2b05be99f73b3e6d1edd
victorperezpiqueras <victorperezpiqueras@gmail.com>
2020-04-07 04:03:23 +0200

    agregados comandos para reproducir audios

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 
 const { Client, MessageEmbed } = require('discord.js');
 const ping = require('minecraft-server-util');
-const mc = require('minecraft-protocol');
+/* const mc = require('minecraft-protocol'); */
+const ytdl = require('ytdl-core');
 
 const bot = new Client();
 
@@ -25,21 +26,55 @@ mcBot.on('chat', function (packet) {
   }
 }); */
 
+function playAudio(msg, audioFile) {
+  if (!msg.guild) return;
+  if (msg.member.voice.channel) {
+    msg.member.voice.channel.join()
+      .then(connection => {
+        connection.play('./audios/' + audioFile + '.mp3');
+      })
+  } else {
+    msg.reply('Conéctate a algun chat de voz tontopolla');
+  }
+}
+
 bot.on('ready', () => {
   console.log('Bot online');
 });
 
 bot.on('message', msg => {
   if (msg.author.equals(bot.user)) return;
-  if (!msg.content.startsWith(PREFIX)) return;
+  if (!msg.content.startsWith(PREFIX)) {
+    switch (msg.content.toLowerCase()) {
+      case '5':
+        msg.reply("por el culo te la hinco");
+        break;
+      case 'cinco':
+        msg.reply("por el culo te la hinco");
+        break;
+      case 'marcelo':
+        var embed = new MessageEmbed()
+          .setImage('https://pbs.twimg.com/media/EQDl1utWoAAMFe6.jpg')
+          .setTitle('agachate y conocelo')
+        msg.channel.send(embed);
+        break;
+      case 'marselo':
+        var embed = new MessageEmbed()
+          .setImage('https://pbs.twimg.com/media/EQDl1utWoAAMFe6.jpg')
+          .setTitle('agachate y conocelo')
+        msg.channel.send(embed);
+        break;
+    }
+  }
 
   let args = msg.content.substring(PREFIX.length).split(' ');
   switch (args[0].toLowerCase()) {
     case 'help':
-      const embed = new MessageEmbed()
+      var embed = new MessageEmbed()
         .setTitle('Comandos')
         .addField('Server info: ', '!server')
         .addField('Chavalada online: ', '!online')
+        .addField('Lista de audios: ', '!audiolist')
         .setColor(0x00AE86)
       msg.channel.send(embed);
       break;
@@ -47,7 +82,7 @@ bot.on('message', msg => {
       ping('51.83.233.138', 25616, (error, response) => {
         if (error) throw error;
         console.log(response)
-        const embed = new MessageEmbed()
+        var embed = new MessageEmbed()
           .setTitle('Server Info')
           .addField('Host: ', response.host)
           .addField('Puerto: ', response.port)
@@ -60,19 +95,15 @@ bot.on('message', msg => {
           if (x + 1 < response.samplePlayers.length) jugadores += ", "
         }
         embed.addField('Chavales jugando: (' + response.onlinePlayers + ')', jugadores)
-
         msg.channel.send(embed);
-
-        /* console.log(response) */
       });
       break;
 
     case 'online':
       ping('51.83.233.138', 25616, (error, response) => {
-
         if (error) throw error;
         console.log(response)
-        const embed = new MessageEmbed()
+        var embed = new MessageEmbed()
           .setTitle('Chavales Online: (' + response.onlinePlayers + ')')
           .setColor(0x00AE86)
         var jugadores = '';
@@ -81,15 +112,43 @@ bot.on('message', msg => {
           if (x + 1 < response.samplePlayers.length) jugadores += ", "
         }
         embed.setDescription(jugadores)
-
         msg.channel.send(embed);
-
-        /* console.log(response) */
       });
       break;
 
-    case '5':
-      msg.author.send("Por el culo te la hinco");
+    case 'audiolist':
+      var embed = new MessageEmbed()
+        .setTitle('Comandos para audios de mierda')
+        .addField('- !eminencia', '\u200B')
+        .addField('- !lesbianas', '\u200B')
+        .addField('- !muybien', '\u200B')
+        .addField('- !quenecesitas', '\u200B')
+        .addField('- !chupateesa', '\u200B')
+        .addField('- !granjascerdos', '\u200B')
+        .addField('- !sinduda', '\u200B')
+        .setColor(0x00AE86)
+      msg.channel.send(embed);
+      break;
+    case 'eminencia':
+      playAudio(msg, 'eminencia');
+      break;
+    case 'lesbianas':
+      playAudio(msg, 'lesbianas');
+      break;
+    case 'muybien':
+      playAudio(msg, 'muybien');
+      break;
+    case 'quenecesitas':
+      playAudio(msg, 'quenecesitas');
+      break;
+    case 'chupateesa':
+      playAudio(msg, 'chupateesa');
+      break;
+    case 'granjascerdos':
+      playAudio(msg, 'granjascerdos');
+      break;
+    case 'sinduda':
+      playAudio(msg, 'sinduda');
       break;
   }
 
