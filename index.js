31231eb19b08b2ee375df1b3712fcf6fe33199fb
victorperezpiqueras <victorperezpiqueras@gmail.com>
2020-04-07 02:17:43 +0200

    agregados comandos de info del server

--- index.js
+++ index.js
@@ -1,16 +1,109 @@
 require('dotenv').config();
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, MessageEmbed } = require('discord.js');
+const ping = require('minecraft-server-util');
+const mc = require('minecraft-protocol');
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+const bot = new Client();
+
+const PREFIX = '!';
+
+/* const mcBot = mc.createClient({
+  host: "51.83.233.138",   // optional
+  port: 25616,         // optional
+  username: "MCBot",
+  version: "1.15.2"
 });
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+mcBot.on('chat', function (packet) {
+  // Listen for chat messages and echo them back.
+  var jsonMsg = JSON.parse(packet.message);
+  if (jsonMsg.translate == 'chat.type.announcement' || jsonMsg.translate == 'chat.type.text') {
+    var username = jsonMsg.with[0].text;
+    var msg = jsonMsg.with[1];
+    if (username === client.username) return;
+    client.write('chat', { message: msg });
   }
+}); */
+
+bot.on('ready', () => {
+  console.log('Bot online');
 });
 
-client.login(process.env.DISCORD_TOKEN);
+bot.on('message', msg => {
+  let args = msg.content.substring(PREFIX.length).split(' ');
+  switch (args[0]) {
+    case 'help':
+      const embed = new MessageEmbed()
+        .setTitle('Comandos')
+        .addField('Server info: ', '!server')
+        .addField('Chavalada online: ', '!online')
+        .setColor(0x00AE86)
+      msg.channel.send(embed);
+      break;
+    case 'server':
+      ping('51.83.233.138', 25616, (error, response) => {
+        if (error) throw error;
+        console.log(response)
+        const embed = new MessageEmbed()
+          .setTitle('Server Info')
+          .addField('Host: ', response.host)
+          .addField('Puerto: ', response.port)
+          .addField('Versión: ', response.version)
+          .addField('Max Players: ', response.maxPlayers)
+          .setColor(0x00AE86)
+        var jugadores = '';
+        for (let x = 0; x < response.samplePlayers.length; x++) {
+          jugadores += response.samplePlayers[x].name
+          if (x + 1 < response.samplePlayers.length) jugadores += ", "
+        }
+        embed.addField('Chavales jugando: (' + response.onlinePlayers + ')', jugadores)
+
+        msg.channel.send(embed);
+
+        /* console.log(response) */
+      });
+
+      break;
+
+    case 'online':
+      ping('51.83.233.138', 25616, (error, response) => {
+
+        if (error) throw error;
+        console.log(response)
+        const embed = new MessageEmbed()
+          .setTitle('Chavales Online: (' + response.onlinePlayers + ')')
+          .setColor(0x00AE86)
+        var jugadores = '';
+        for (let x = 0; x < response.samplePlayers.length; x++) {
+          jugadores += response.samplePlayers[x].name
+          if (x + 1 < response.samplePlayers.length) jugadores += ", "
+        }
+        embed.setDescription(jugadores)
+
+        msg.channel.send(embed);
+
+        /* console.log(response) */
+      });
+
+      break;
+  }
+
+});
+
+/* const mcBot = mc.createClient({
+  host: "51.83.233.138",   // optional
+  port: 25616,         // optional
+  username: "MCBot",
+});
+mcBot.on('chat', function (packet) {
+  // Listen for chat messages and echo them back.
+  var jsonMsg = JSON.parse(packet.message);
+  if (jsonMsg.translate == 'chat.type.announcement' || jsonMsg.translate == 'chat.type.text') {
+    var username = jsonMsg.with[0].text;
+    var msg = jsonMsg.with[1];
+    if (username === client.username) return;
+    client.write('chat', { message: msg });
+  }
+}); */
+
+bot.login(process.env.DISCORD_TOKEN);
